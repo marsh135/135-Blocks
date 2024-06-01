646818d7c9e8e2f959256769e022746b0b4b3f64
PaidShill <112586790+PaidShill@users.noreply.github.com>
2024-06-01 16:29:39 -0400

    REV Mecanum Needs to be tested
    Going to see if renaming all the folders fixes our compile issue in personal save

--- src/main/java/frc/robot/subsystems/drive/Mecanum/REVMecanumS.java
+++ src/main/java/frc/robot/subsystems/drive/Mecanum/REVMecanumS.java
@@ -7,6 +7,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N2;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
@@ -35,6 +36,7 @@ import com.pathplanner.lib.util.ReplanningConfig;
 import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkFlex;
 import edu.wpi.first.math.system.LinearSystem;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Time;
@@ -51,23 +53,24 @@ public class REVMecanumS implements DrivetrainS {
 	//TODO: mecanum sim, pathPlanner support
 	private static CANSparkBase[] sparkMotors = new CANSparkBase[4];
 	private static int[] motorIDs;
-	private static double[] wheelSimPositions = { 0, 0, 0, 0
-	}, wheelSimVelocities = { 0, 0, 0, 0
-	};
-	private static LinearSystem<N1, N1, N1>[] linearSystems = new LinearSystem[4];
-	private static KalmanFilter<N1,N1,N1>[] wheelFilters = new KalmanFilter[4];
-	private static LinearQuadraticRegulator<N1,N1,N1>[] wheelRegulators = new LinearQuadraticRegulator[4];
-	private static LinearSystemLoop<N1,N1,N1>[] wheelSystemLoops = new LinearSystemLoop[4]; 
-	
+	@SuppressWarnings("unchecked")
+	private static LinearSystem<N2, N1, N2>[] linearSystems = new LinearSystem[4];
+	@SuppressWarnings("unchecked")
+	private static KalmanFilter<N2, N1, N2>[] wheelFilters = new KalmanFilter[4];
+	@SuppressWarnings("unchecked")
+	private static LinearQuadraticRegulator<N2, N1, N2>[] wheelRegulators = new LinearQuadraticRegulator[4];
+	@SuppressWarnings("unchecked")
+	private static LinearSystemLoop<N2, N1, N2>[] wheelSystemLoops = new LinearSystemLoop[4];
+	private static DCMotorSim[] motorSims = new DCMotorSim[4];
 	private static RelativeEncoder[] wheelRelativeEncoders = new RelativeEncoder[4];
 	private static AHRS gyro;
 	private static MecanumDriveKinematics driveKinematics;
 	private static MecanumDrivePoseEstimator drivePoseEstimator;
 	private static MotorConstantContainer[] wheelConstantContainers = new MotorConstantContainer[4];
 	private static double maxDriveVelMetersPerSec;
-	private static Pose2d pose = new Pose2d(0,0,new Rotation2d(0));
-	private static MecanumDriveWheelSpeeds speeds = new MecanumDriveWheelSpeeds(0,0,0,0);
-	private static MecanumDriveWheelPositions positions = new MecanumDriveWheelPositions(0,0,0,0);
+	private static double[] desiredVelocities = {0,0,0,0};
+	private static double[] currentPositions = {0,0,0,0};
+	private static Pose2d pose = new Pose2d(0, 0, new Rotation2d(0));
 	Measure<Velocity<Voltage>> rampRate = Volts.of(1).per(Seconds.of(1)); //for going FROM ZERO PER SECOND
 	Measure<Voltage> holdVoltage = Volts.of(4);
 	Measure<Time> timeout = Seconds.of(10);
@@ -137,20 +140,42 @@ public class REVMecanumS implements DrivetrainS {
 			default:
 				throw new UnsupportedOperation("no REV motortype found");
 			}
+			linearSystems[i] = LinearSystemId
+					.createDCMotorSystem(wheelConstantContainers[i].getKv(), wheelConstantContainers[i].getKa());
 			sparkMotors[i].setIdleMode(IdleMode.kBrake);
 			sparkMotors[i].enableVoltageCompensation(12);
 			sparkMotors[i].setSmartCurrentLimit(i, i);
 			sparkMotors[i].clearFaults();
 			sparkMotors[i].burnFlash();
-			wheelRelativeEncoders[i] = sparkMotors[i].getEncoder();
-			wheelRelativeEncoders[i]
-					.setPositionConversionFactor(1 / gearing * kWheelRadiusMeters);
-			linearSystems[i] = LinearSystemId.identifyVelocitySystem(wheelConstantContainers[i].getKv(), wheelConstantContainers[i].getKa());
-			wheelFilters[i] = new KalmanFilter<N1,N1,N1>(Nat.N1(), Nat.N1(), linearSystems[i], VecBuilder.fill(3), VecBuilder.fill(.03), .02);
-			wheelRegulators[i] = new LinearQuadraticRegulator<N1,N1,N1>(linearSystems[i], VecBuilder.fill(10), VecBuilder.fill(12), .02);
-			wheelSystemLoops[i] = new LinearSystemLoop<>(linearSystems[i],wheelRegulators[i],wheelFilters[i],12,.02);
+							wheelRelativeEncoders[i] = sparkMotors[i].getEncoder();
+				wheelRelativeEncoders[i].setPositionConversionFactor(
+						1 / gearing * kWheelRadiusMeters);
+				wheelFilters[i] = new KalmanFilter<N2, N1, N2>(Nat.N2(), Nat.N2(),
+						linearSystems[i], VecBuilder.fill(3, 3),
+						VecBuilder.fill(.03, 3), .02);
+				wheelRegulators[i] = new LinearQuadraticRegulator<N2, N1, N2>(
+						linearSystems[i], VecBuilder.fill(10, 10),
+						VecBuilder.fill(12), .02);
+				wheelSystemLoops[i] = new LinearSystemLoop<>(linearSystems[i],
+						wheelRegulators[i], wheelFilters[i], 12, .02);
+			switch (Constants.currentMode) {
+
+			case SIM:
+				switch (DriveConstants.robotMotorController) {
+				case NEO_SPARK_MAX:
+					motorSims[i] = new DCMotorSim(linearSystems[i],
+							DCMotor.getNEO(1), gearing);
+					break;
+				case VORTEX_SPARK_FLEX:
+					motorSims[i] = new DCMotorSim(linearSystems[i],
+							DCMotor.getNeoVortex(maxAmps), gearing);
+				default:
+					break;
+				}
+			default:
+				break;
+			}
 		}
-		
 		gyro = new AHRS(Port.kUSB);
 		driveKinematics = new MecanumDriveKinematics(
 				DriveConstants.kModuleTranslations[0],
@@ -175,25 +200,25 @@ public class REVMecanumS implements DrivetrainS {
 
 	public MecanumDriveWheelSpeeds getWheelSpeeds() {
 		switch (Constants.currentMode) {
-			case SIM:
-				return new MecanumDriveWheelSpeeds(wheelSimVelocities[0],wheelSimVelocities[1],wheelSimVelocities[2],wheelSimVelocities[3]);
-
-		
-			default:
-					return new MecanumDriveWheelSpeeds(wheelRelativeEncoders[0].getVelocity(),
-				wheelRelativeEncoders[1].getVelocity(),
-				wheelRelativeEncoders[2].getVelocity(),
-				wheelRelativeEncoders[3].getVelocity());				
+		case SIM:
+			return new MecanumDriveWheelSpeeds(motorSims[0].getAngularVelocityRadPerSec(),
+					motorSims[1].getAngularVelocityRadPerSec(), motorSims[2].getAngularVelocityRadPerSec(),
+					motorSims[3].getAngularVelocityRadPerSec());
+		default:
+			return new MecanumDriveWheelSpeeds(
+					wheelRelativeEncoders[0].getVelocity(),
+					wheelRelativeEncoders[1].getVelocity(),
+					wheelRelativeEncoders[2].getVelocity(),
+					wheelRelativeEncoders[3].getVelocity());
 		}
-
 	}
 
 	public MecanumDriveWheelPositions getWheelPositions() {
 		switch (Constants.currentMode) {
 		case SIM:
-			return new MecanumDriveWheelPositions(wheelSimPositions[0],
-					wheelSimPositions[1], wheelSimPositions[2],
-					wheelSimPositions[3]);
+			return new MecanumDriveWheelPositions(motorSims[0].getAngularPositionRad(),
+					motorSims[1].getAngularPositionRad(), motorSims[2].getAngularPositionRad(),
+					motorSims[3].getAngularPositionRad());
 		default:
 			return new MecanumDriveWheelPositions(
 					wheelRelativeEncoders[0].getPosition(),
@@ -205,22 +230,33 @@ public class REVMecanumS implements DrivetrainS {
 
 	@Override
 	public void periodic() {
-		for (int i = 0; i < 4; i++){
-	
+		
+		for (int i = 0; i < 4; i++) {
+			wheelSystemLoops[i].setNextR(VecBuilder.fill(desiredVelocities[i], currentPositions[i]+desiredVelocities[i]*.2));
+			switch (Constants.currentMode) {
+				case REAL:
+				wheelSystemLoops[i].correct(VecBuilder.fill(wheelRelativeEncoders[i].getPosition(), wheelRelativeEncoders[i].getVelocity()));
+				wheelSystemLoops[i].predict(.02);
+				sparkMotors[i].setVoltage(wheelSystemLoops[i].getU(0));
+					break;
+				case SIM:
+				motorSims[i].update(.02);
+				wheelSystemLoops[i].predict(.02);
+				motorSims[i].setInputVoltage(wheelSystemLoops[i].getU(0));
+				default:
+					break;
+			}
+
 		}
 	}
 
 	@Override
 	public void setChassisSpeeds(ChassisSpeeds speeds) {
 		MecanumDriveWheelSpeeds indSpeeds = driveKinematics.toWheelSpeeds(speeds);
-		sparkMotors[0]
-				.set(indSpeeds.frontLeftMetersPerSecond / maxDriveVelMetersPerSec);
-		sparkMotors[1]
-				.set(indSpeeds.frontRightMetersPerSecond / maxDriveVelMetersPerSec);
-		sparkMotors[2]
-				.set(indSpeeds.rearLeftMetersPerSecond / maxDriveVelMetersPerSec);
-		sparkMotors[3]
-				.set(indSpeeds.rearRightMetersPerSecond / maxDriveVelMetersPerSec);
+		desiredVelocities[0] = indSpeeds.frontLeftMetersPerSecond;
+		desiredVelocities[1] = indSpeeds.frontRightMetersPerSecond;
+		desiredVelocities[2] = indSpeeds.rearLeftMetersPerSecond;
+		desiredVelocities[3] = indSpeeds.rearRightMetersPerSecond;
 	}
 
 	@Override
@@ -245,7 +281,6 @@ public class REVMecanumS implements DrivetrainS {
 
 	@Override
 	public void stopModules() {
-		
 		for (int i = 0; i < 4; i++) {
 			sparkMotors[i].set(0);
 		}
