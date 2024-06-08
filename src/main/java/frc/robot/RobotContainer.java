e18c3408825242dcf39fcc41dd577b2fc34ee774
Eaglegrant <grantman522@gmail.com>
2024-06-07 21:47:52 -0400

    realistic currentDrawDrive

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -197,7 +197,7 @@ public class RobotContainer {
 	 */
 	public static double[] getCurrentDraw(){
 		return new double[]{
-			drivetrainS.getCurrent()
+			Math.min(drivetrainS.getCurrent(),200)
 		};
 	}
 	public static BooleanSupplier isDriving() {
